4bbdb32b128081074a769699423a7a5b90969f27
PGaur <PGaur@PGaur-Laptop>
2017-08-25 05:31:03 +0530

    Commit for Final Game

--- 2DSnake/src/com/snake/Gameplay.java
+++ 2DSnake/src/com/snake/Gameplay.java
@@ -1,12 +1,13 @@
 package com.snake;
 
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
-
+import java.util.Random;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -33,6 +34,21 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 	private int delay = 100;
 	private ImageIcon snakeimage;
 	
+	private int[] enemyxpos = {25,50,75,100,125,150,175,200,225,250,275,300,325,
+			350,375,400,425,450,475,500,525,550,575,600,625,650,
+			675,700,725,750,775,800,825,850};
+	private int[] enemyypos = {75,100,125,150,175,200,225,250,275,300,325,350,
+			375,400,425,450,475,500,525,550,575,600,625};
+	
+	private ImageIcon enemyimage;
+	
+	private Random random = new Random();
+	
+	private int xpos = random.nextInt(34);
+	private int ypos = random.nextInt(23);
+	
+	private int score = 0;
+	
 	private int moves = 0; 
 	
 	private ImageIcon titleImage;
@@ -54,8 +70,6 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 			snakeylength[2] = 100;
 			snakeylength[1] = 100;
 			snakeylength[0] = 100;
-			
-			//31 minutes
 		}
 		
 		//Draw title image border
@@ -73,6 +87,16 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 		//Draw background for the gameplay
 		g.setColor(Color.black);
 		g.fillRect(25, 75, 850, 575);
+		
+		//Display Scores
+		g.setColor(Color.white);
+		g.setFont(new Font("arial", Font.PLAIN, 14));
+		g.drawString("Scores : "+score, 780, 30);
+		
+		//Display Length
+		g.setColor(Color.white);
+		g.setFont(new Font("arial", Font.PLAIN, 14));
+		g.drawString("Length : "+lengthofsanke , 780, 50);
 		
 		rightmouth = new ImageIcon("res/rightmouth.png");
 		rightmouth.paintIcon(this, g, snakexlength[0], snakeylength[0]);
@@ -104,26 +128,182 @@ public class Gameplay extends JPanel implements KeyListener, ActionListener{
 			}
 		}
 		
+		enemyimage = new ImageIcon("res/enemy.png");
+		
+		if(enemyxpos[xpos] == snakexlength[0] && enemyypos[ypos] == snakeylength[0]){
+			score++;
+			lengthofsanke++;
+			xpos = random.nextInt(34);
+			ypos = random.nextInt(23);
+		}
+		
+		enemyimage.paintIcon(this, g, enemyxpos[xpos], enemyypos[ypos]);
+		
+		for(int collision = 1; collision < lengthofsanke; collision++){
+			if(snakexlength[collision] == snakexlength[0] && snakeylength[collision] == snakeylength[0]){
+				right = false;
+				left = false;
+				up = false;
+				down = false;
+				
+				g.setColor(Color.white);
+				g.setFont(new Font("arial", Font.BOLD, 50));
+				g.drawString("Game Over", 300, 300);
+				
+				g.setFont(new Font("arial", Font.BOLD, 20));
+				g.drawString("Press SPACE to RESTART", 310, 340);
+			}
+		}
 		g.dispose();
 	}
 	
 	@Override
-	public void actionPerformed(ActionEvent arg0) {
+	public void actionPerformed(ActionEvent e) {
+		timer.start();
+		if(right){
+			for(int r = lengthofsanke-1; r >=0; r--){
+				snakeylength[r+1] = snakeylength[r];
+			}
+			for(int r = lengthofsanke-1; r >=0; r--){
+				if(r==0){
+					snakexlength[r] = snakexlength[r] + 25;
+				} else{
+					snakexlength[r] = snakexlength[r-1];
+				}
+				if(snakexlength[r] > 850){
+					snakexlength[r] = 25;
+				}
+			}
+			repaint();
+		}
+		
+		if(left){
+			for(int r = lengthofsanke-1; r >=0; r--){
+				snakeylength[r+1] = snakeylength[r];
+			}
+			for(int r = lengthofsanke-1; r >=0; r--){
+				if(r==0){
+					snakexlength[r] = snakexlength[r] - 25;
+				} else{
+					snakexlength[r] = snakexlength[r-1];
+				}
+				if(snakexlength[r] < 25){
+					snakexlength[r] = 850;
+				}
+			}
+			repaint();
+		}
+		
+		if(up){
+			for(int r = lengthofsanke-1; r >=0; r--){
+				snakexlength[r+1] = snakexlength[r];
+			}
+			for(int r = lengthofsanke-1; r >=0; r--){
+				if(r==0){
+					snakeylength[r] = snakeylength[r] - 25;
+				} else{
+					snakeylength[r] = snakeylength[r-1];
+				}
+				if(snakeylength[r] < 75){
+					snakeylength[r] = 625;
+				}
+			}
+			repaint();
+		}
+		
+		if(down){
+			for(int r = lengthofsanke-1; r >=0; r--){
+				snakexlength[r+1] = snakexlength[r];
+			}
+			for(int r = lengthofsanke-1; r >=0; r--){
+				if(r==0){
+					snakeylength[r] = snakeylength[r] + 25;
+				} else{
+					snakeylength[r] = snakeylength[r-1];
+				}
+				if(snakeylength[r] > 625){
+					snakeylength[r] = 75;
+				}
+			}
+			repaint();
+		}
+		
 		
 	}
 	
 	@Override
-	public void keyTyped(KeyEvent arg0) {
+	public void keyTyped(KeyEvent e) {
 		
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode() == KeyEvent.VK_RIGHT){
+			moves++;
+			right = true;
+			if(!left){
+				right=true;
+			} else{
+				right = false;
+				left = true;
+			}
+
+			up = false;
+			down = false;
+		}
 		
+		if(e.getKeyCode() == KeyEvent.VK_LEFT){
+			moves++;
+			left = true;
+			if(!right){
+				left=true;
+			} else{
+				left = false;
+				right = true;
+			}
+
+			up = false;
+			down = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_UP){
+			moves++;
+			up = true;
+			if(!down){
+				up=true;
+			} else{
+				up = false;
+				down = true;
+			}
+
+			left = false;
+			right = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_DOWN){
+			moves++;
+			down = true;
+			if(!up){
+				down=true;
+			} else{
+				down = false;
+				up = true;
+			}
+
+			left = false;
+			right = false;
+		}
+		
+		if(e.getKeyCode() == KeyEvent.VK_SPACE){
+			moves = 0;
+			score = 0;
+			lengthofsanke = 3;
+			repaint();
+		}
 	}
 
 	@Override
-	public void keyReleased(KeyEvent arg0) {
+	public void keyReleased(KeyEvent e) {
 		
 	}
 }
